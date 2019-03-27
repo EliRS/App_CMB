7e2f4b520fe5dc1871d3a92ada2b3056c54c7fc3
EliRS <eliasrodriguesdossantos100@gmail.com>
2019-03-27 09:27:39 +0900

    ajustes na função calcular

--- www/components/acoes.js
+++ www/components/acoes.js
@@ -5,18 +5,21 @@ $(document).on('keyup','#numeric',function(){
 $(document).on('click','#igual',function(){
   var sexo = $('input[name=sexo]:checked').val();
   var taxa = parseFloat($('input[name=taxa]:checked').val());
-  var altura = parseFloat($("#altura").val());
-  var idade = parseFloat($('.idade').val());
-  var peso = parseFloat($('.peso').val());
-  var mb;
-  if(sexo == "F"){
-    mb =  taxa * (655 + ((9.6 * peso )+ (1.8 * altura) - (4.7 * idade)));
+  if($("#altura").val()==""||$('.idade').val()=="" || $('.peso').val()==""){
+    $("#mss").text("Preencha todos os campos para continuar ");
   }else{
-    mb =  taxa * (66 + ((13.7 * peso )+ (5 * altura) - (6.8 * idade)));
+    var altura = parseFloat($("#altura").val());
+    var idade = parseFloat($('.idade').val());
+    var peso = parseFloat($('.peso').val());
+    var mb;
+    if(sexo == "F"){
+      mb =  taxa * (655 + ((9.6 * peso )+ (1.8 * altura) - (4.7 * idade)));
+    }else{
+      mb =  taxa * (66 + ((13.7 * peso )+ (5 * altura) - (6.8 * idade)));
+    }
+    $("#mss").text("Quantidade Que o seu corpo Precisa em 24 é de " + mb);
   }
-  $("#mss").text("Quantidade Que o seu corpo Precisa em 24 é de " + mb);
 });
-
 function sumiu(){
   document.getElementById('txtSexo').hidden = false;
 }
