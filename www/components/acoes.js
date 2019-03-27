fabdf290935b0a6681d16f7b943ae45b06a1ee3b
EliRS <eliasrodriguesdossantos100@gmail.com>
2019-03-27 09:26:19 +0900

    Funcionalidade do botão calcular

--- www/components/acoes.js
+++ www/components/acoes.js
@@ -2,6 +2,21 @@ $(document).on('keyup','#numeric',function(){
   $(this).val(this.value.replace(/\D/g, ''));
 });
 
+$(document).on('click','#igual',function(){
+  var sexo = $('input[name=sexo]:checked').val();
+  var taxa = parseFloat($('input[name=taxa]:checked').val());
+  var altura = parseFloat($("#altura").val());
+  var idade = parseFloat($('.idade').val());
+  var peso = parseFloat($('.peso').val());
+  var mb;
+  if(sexo == "F"){
+    mb =  taxa * (655 + ((9.6 * peso )+ (1.8 * altura) - (4.7 * idade)));
+  }else{
+    mb =  taxa * (66 + ((13.7 * peso )+ (5 * altura) - (6.8 * idade)));
+  }
+  $("#mss").text("Quantidade Que o seu corpo Precisa em 24 é de " + mb);
+});
+
 function sumiu(){
   document.getElementById('txtSexo').hidden = false;
 }
